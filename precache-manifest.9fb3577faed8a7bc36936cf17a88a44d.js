490249a813756d31971f7d70f8b2b073a0806c21
Ketan Rajpal <ketanrajpal@gmail.com>
2020-05-17 02:31:05 +0530

    Updates

---
+++ precache-manifest.9fb3577faed8a7bc36936cf17a88a44d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b452e34942517d23cdc7c3ce279955d",
+    "url": "/mileage.in/index.html"
+  },
+  {
+    "revision": "a16afc398ab866de0c58",
+    "url": "/mileage.in/static/css/main.d523364e.chunk.css"
+  },
+  {
+    "revision": "6c5e4c536b8a5b36a65c",
+    "url": "/mileage.in/static/js/2.4e5837c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mileage.in/static/js/2.4e5837c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16afc398ab866de0c58",
+    "url": "/mileage.in/static/js/main.142ada85.chunk.js"
+  },
+  {
+    "revision": "8929a5e4e3c193ba2dd9",
+    "url": "/mileage.in/static/js/runtime-main.b04d062d.js"
+  },
+  {
+    "revision": "e18b133ef44c7fecd8fd978b9bceac7d",
+    "url": "/mileage.in/static/media/artwork.e18b133e.svg"
+  },
+  {
+    "revision": "13bb364a13363f7562ff1fb34c63f26f",
+    "url": "/mileage.in/static/media/corporate-and-brand-public-relations.13bb364a.svg"
+  },
+  {
+    "revision": "df058f4224dcc22f1453e91296a2ba83",
+    "url": "/mileage.in/static/media/crisis-communictions.df058f42.svg"
+  },
+  {
+    "revision": "2c46115dc81969085c3e261479f3386b",
+    "url": "/mileage.in/static/media/event-management.2c46115d.svg"
+  },
+  {
+    "revision": "da3cf3d0e7b65c6041a8935318969767",
+    "url": "/mileage.in/static/media/influenser-and-blogger-engagement.da3cf3d0.svg"
+  },
+  {
+    "revision": "90200bbbfb09fea5c631404f55b4a12e",
+    "url": "/mileage.in/static/media/logo.90200bbb.png"
+  },
+  {
+    "revision": "cf0b69876029cd789e3e3284fe91b90e",
+    "url": "/mileage.in/static/media/marketing-management.cf0b6987.svg"
+  },
+  {
+    "revision": "e5d7b657304a2ebfea64a9b95be4be9c",
+    "url": "/mileage.in/static/media/media-training.e5d7b657.svg"
+  },
+  {
+    "revision": "c4507a4ae839304337539985c4e7c776",
+    "url": "/mileage.in/static/media/reputation-management.c4507a4a.svg"
+  },
+  {
+    "revision": "f06da36ed201b11c771eeb75b11b7458",
+    "url": "/mileage.in/static/media/social-and-digital-marketing.f06da36e.svg"
+  }
+]);
