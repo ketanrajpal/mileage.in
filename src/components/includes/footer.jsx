5b06f811784e3517100adce3263e31bf03f6ae50
Ketan Rajpal <ketanrajpal@gmail.com>
2020-08-28 02:39:30 +0530

    footer

---
+++ src/components/includes/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer>
+      <div className="copyright">2020 &copy; About Mileage Communications Group.</div>
+      <div>
+        <span>
+          Email. <a href="mailto:info@mileage.in">info@mileage.in</a>
+        </span>
+        <span>
+          Phone. <a href="tel:+917290050770">+ (91) 72900-50770</a>
+        </span>
+      </div>
+    </footer>
+  );
+};
