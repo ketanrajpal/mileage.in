2010115533f2cd5f2c51ca28914f281e8d6b72d2
Ketan Rajpal <ketanrajpal@gmail.com>
2020-08-28 02:40:28 +0530

    Website version 0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { HashRouter, Switch, Route } from "react-router-dom";
+
+import HeaderComponent from "./components/includes/header";
+import FooterComponent from "./components/includes/footer";
+
+import IndexComponent from "./components/index";
+import AboutComponent from "./components/about";
+import ExpertiseComponent from "./components/expertise";
+import TeamComponent from "./components/team";
+import ContactComponent from "./components/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <HeaderComponent></HeaderComponent>
+        <Switch>
+          <Route path="/" exact>
+            <IndexComponent></IndexComponent>
+          </Route>
+          <Route path="/about">
+            <AboutComponent></AboutComponent>
+          </Route>
+          <Route path="/expertise">
+            <ExpertiseComponent></ExpertiseComponent>
+          </Route>
+          <Route path="/team">
+            <TeamComponent></TeamComponent>
+          </Route>
+          <Route path="/contact">
+            <ContactComponent></ContactComponent>
+          </Route>
+        </Switch>
+        <FooterComponent></FooterComponent>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/components/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const IndexComponent = () => {
+  return (
+    <div className="index">
+      <section className="animate__animated animate__zoomIn">
+        <div className="description">
+          <h1>The Mileage Communications Group</h1>
+          <h2>We go the extra mile for you</h2>
+          <p>
+            A full-service strategic marketing communications, PR and representation company. An equity joint venture with Mileage Communications,
+            Singapore. A member of Pinnacle Worldwide - a global network of PR agencies
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default IndexComponent;
